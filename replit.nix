8eba0dafc7dd92842e4ca7e86a29bb1a5666de0b
Innocentpandav <pandaexpress564@gmail.com>
2025-08-30 20:58:23 +0000

    Fix

--- replit.nix
+++ replit.nix
@@ -1,17 +1,7 @@
 { pkgs }: {
   deps = [
-    pkgs.tesseract
-    pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
-    pkgs.replitPackages.stderred
+    pkgs.python312Full   # use Python 3.12 to match Replit default
+    pkgs.tesseract       # install Tesseract OCR
+    pkgs.git             # sometimes useful for pulling updates
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.tesseract
-    ];
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
-    LANG = "en_US.UTF-8";
-    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
-  };
 }
